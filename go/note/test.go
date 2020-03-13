0def7b1c04e1ae105000d4263f86363e8cc4eb61
zygoodest <zygoodest@126.com>
2020-03-13 23:31:46 +0800

    update go nodes

--- go/note/test.go
+++ go/note/test.go
@@ -1,25 +1,10 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
 
-func f(s string) {
-	defer func() {
-		err := recover()
-		if err != nil {
-			fmt.Printf("出现异常，异常为: %v\n", err)
-		}
-	}()
-	if s == "p" {
-		panic(errors.New("自定义错误"))
+func Sum(set []int) int {
+	var result int
+	for _, num := range set {
+		result += num
 	}
-	fmt.Println(s)
-}
-
-func main() {
-	f("11111")
-	f("p")
-	f("22222")
+	return result
 }
